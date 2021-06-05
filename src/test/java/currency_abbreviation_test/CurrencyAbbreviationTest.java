78b451ed779a7692cc4a08c336e3addd1e926097
greendead140989 <greendead140989@gmail.com>
2021-06-05 20:02:10 +0300

    I add comment to Kostya 2m

--- src/test/java/currency_abbreviation_test/CurrencyAbbreviationTest.java
+++ src/test/java/currency_abbreviation_test/CurrencyAbbreviationTest.java
@@ -11,22 +11,22 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CurrencyAbbreviationTest {
-
+    //I add comenta
     @DataProvider(name = "data-provider")
-    public Object[][]dpMethod(){
+    public Object[][] dpMethod() {
         return new Object[][]{
-                {298,"RUB"},
-                {1,"ALL"},
-                {145,"USD"}};
+                {298, "RUB"},
+                {1, "ALL"},
+                {145, "USD"}};
     }
 
-        @Test(dataProvider = "data-provider")
-        public void currencyAbbreviationTest ( int id, String cur_Abbreviation){
-            RestAssured.baseURI = "https://www.nbrb.by/api/exrates/currencies/" + id;
-            RequestSpecification httpReguest = RestAssured.given();
-            Response response = httpReguest.request(Method.GET);
-            CurrencyOll currencyOll = response.getBody().as(CurrencyOll.class);
-            String resultAbbreviation = currencyOll.cur_Abbreviation;
-            Assert.assertEquals(resultAbbreviation, cur_Abbreviation, "Currency rate is not correct");
-        }
+    @Test(dataProvider = "data-provider")
+    public void currencyAbbreviationTest(int id, String cur_Abbreviation) {
+        RestAssured.baseURI = "https://www.nbrb.by/api/exrates/currencies/" + id;
+        RequestSpecification httpReguest = RestAssured.given();
+        Response response = httpReguest.request(Method.GET);
+        CurrencyOll currencyOll = response.getBody().as(CurrencyOll.class);
+        String resultAbbreviation = currencyOll.cur_Abbreviation;
+        Assert.assertEquals(resultAbbreviation, cur_Abbreviation, "Currency rate is not correct");
     }
+}
